2ad64116ed229156118271133e9a41881b9a2888
Julian Alvarez <alvarez.julian@correounivalle.edu.co>
2021-10-13 16:21:08 -0500

    He creado un nuevo Thread y he implementado Runnable y run - 03

--- Juego/src/juego/Juego.java
+++ Juego/src/juego/Juego.java
@@ -5,15 +5,19 @@ import java.awt.Canvas;
 import java.awt.Dimension;
 import javax.swing.JFrame;
 
-public class Juego extends Canvas {
+public class Juego extends Canvas implements Runnable{
     
     private static JFrame ventana;
+    private static Thread thread;
+    
     private static final int ANCHO = 800;
     private static final int ALTO=600;
     private static final String NOMBRE = "Juego";
     
+    //Hola
+    
     private Juego(){
-        setPreferredSize(new Dimension(ANCHO,ALTO));
+        setPreferredSize(new Dimension(ANCHO,ALTO));    
         
         ventana = new JFrame(NOMBRE);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -22,10 +26,29 @@ public class Juego extends Canvas {
         ventana.add(this, BorderLayout.CENTER);
         ventana.pack();
         ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
         }
+    
     
     public static void main(String[] args){
         Juego juego = new Juego();
+        juego.iniciar();
+    }
+    
+    private void iniciar(){
+        thread = new Thread(this, "Graficos");
+        thread.start();
+    
+    }
+    
+    private void detener(){
+    
+    }
+
+    @Override
+    public void run() {
+        System.out.println("El thread 2 se está ejecutando con exito");
+        
     }
 
     

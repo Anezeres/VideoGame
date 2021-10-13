c11b284bc0c50ba4e9fcc346e96b5fd7dbedae7b
Julian Alvarez <alvarez.julian@correounivalle.edu.co>
2021-10-13 14:40:14 -0500

    Creacion de la ventana en la clase Juego - 02

--- Juego/src/juego/Juego.java
+++ Juego/src/juego/Juego.java
@@ -1,21 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package juego;
 
-/**
- *
- * @author Usuario
- */
-public class Juego {
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Juego extends Canvas {
+    
+    private static JFrame ventana;
+    private static final int ANCHO = 800;
+    private static final int ALTO=600;
+    private static final String NOMBRE = "Juego";
+    
+    private Juego(){
+        setPreferredSize(new Dimension(ANCHO,ALTO));
+        
+        ventana = new JFrame(NOMBRE);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setResizable(false);
+        ventana.setLayout(new BorderLayout());
+        ventana.add(this, BorderLayout.CENTER);
+        ventana.pack();
+        ventana.setLocationRelativeTo(null);
+        }
+    
+    public static void main(String[] args){
+        Juego juego = new Juego();
     }
+
+    
     
 }

cd8ef57609e834a36205437b962be15cec86ba69
Julian Alvarez <alvarez.julian@correounivalle.edu.co>
2021-10-14 00:06:48 -0500

    Se crea la Clase pantalla, con metodo limpiar y metodo mostrar con un doble bucle anidado y unos condicionales para evitar errores y no dibujar pixeles fuera de la pantalla- 10

---
+++ Juego/src/graficos/Pantalla.java
@@ -0,0 +1,49 @@
+package graficos;
+
+public final class Pantalla {
+    
+    private final int ancho;
+    private final int alto;
+    
+    public final int pixeles[];
+    
+    public Pantalla(final int ancho, final int alto){
+        this.ancho = ancho;
+        this.alto = alto;
+        
+        pixeles = new int[ancho*alto];
+    
+    }
+    
+    public void limpiar(){
+        for (int i = 0; i < pixeles.length; i++) {
+            pixeles[i] = 0;
+            
+        }
+    }
+    
+    public void mostar(final int compensacionX, final int compensacionY){
+        
+        for (int y = 0; y < alto; y++) {
+            int posicionY = y + compensacionY;
+            
+            if(posicionY < 0 || posicionY > alto){
+                continue;
+            }
+            for (int x = 0; x < ancho; x++) {
+                int posicionX = y + compensacionX; 
+                
+                if(posicionX<0 || posicionX>ancho){
+                continue;
+                }
+                
+                //Codigo para redibujar la pantalla
+                
+            }
+            
+        }
+    
+    }
+    
+    
+}
